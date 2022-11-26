4628996873a1dbc7963275f9608e1122a78e78d5
paulomv <paulomv29@outlook.com>
2022-11-26 17:50:57 -0300

    Boas práticas aplicadas a organização do css

--- src/components /Carousel/styles.js
+++ src/components /Carousel/styles.js
@@ -1,26 +1,38 @@
 import styled from 'styled-components';
 
 export const Title = styled.h3`
+  display: inline-block;
+  
+  margin-bottom: 16px;
+  padding: 20px;
+
+  background: red;
+  border-radius: 4px;
+  
   font-style: normal;
   font-weight: normal;
   font-size: 35px;
   line-height: 1;
-  margin-bottom: 16px;
-  display: inline-block;
-  padding: 20px;
-  background: red;
-  line-height: 1;
-  border-radius: 4px;
+
+  cursor: default;
+  transition: scale 0.3s;
 
   @media (max-width: 800px) {
-    font-size: 18px;
     padding: 10px;
+    
+    font-size: 18px;
+  }
+
+  &:hover {
+    scale: 1.1;
   }
 `;
 
 export const ExtraLink = styled.a`
   margin-left: 16px;
+
   text-decoration: none;
+
   transition: opacity .3s;
   &:hover,
   &:focus {
@@ -28,19 +40,22 @@ export const ExtraLink = styled.a`
   }
   @media (max-width: 800px) {
     display: block;
+
     margin-bottom: 16px;
     margin-left: 0;
   }
 `;
 
 export const VideoCardList = styled.ul`
-  margin: 0;
-  padding-left: 0;
-  padding-bottom: 32px;
-  list-style: none;
   display: flex;
   overflow-x: auto;
   flex-direction: row;
+
+  margin: 0;
+  padding-left: 0;
+  padding-bottom: 32px;
+
+  list-style: none;
   
   li {
     margin-right: 16px;
@@ -48,8 +63,11 @@ export const VideoCardList = styled.ul`
 `;
 
 export const VideoCardGroupContainer = styled.section`
-  color: white;
-  min-height: 197px;
   margin-left: 5%;
+  margin-right: 5%;
   margin-bottom: 16px;
+  
+  min-height: 197px;
+  
+  color: white;
 `;
