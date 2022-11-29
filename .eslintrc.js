ca44c9caea7e623f2ec797044feceb99504df9ff
paulomv <paulomv29@outlook.com>
2022-11-28 23:35:14 -0300

    mundaça nas configurações do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,13 @@ module.exports = {
   ],
   rules: {
     'react/forbid-prop-types': 0,
+    'no-console': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'CallExpression[callee.object.name="console"][callee.property.name!=/^(log|warn|error|info|trace)$/]',
+        message: 'Unexpected property on console object was called',
+      }
+    ]
   },
 };
